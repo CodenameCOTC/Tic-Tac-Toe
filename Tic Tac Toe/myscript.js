6d4b522823156e51f3257da7c02d496aa6e68f00
A. Noval Adip <33487372+CodenameCOTC@users.noreply.github.com>
2017-11-21 22:03:32 +0700

    Add files via upload

---
+++ Tic Tac Toe/myscript.js
@@ -0,0 +1,28 @@
+// Restart game button
+var restart = document.querySelector("#b");
+//Grabs all the squares
+var squares = document.querySelectorAll('td');
+
+//Clear all the squares
+function clearBoard() {
+  for (var i = 0; i < squares.length; i++) {
+    squares[i].textContent = '';
+  }
+}
+
+restart.addEventListener('click', clearBoard);
+//Check the squares marker
+function changeMarker(){
+  if(this.textContent === '') {
+    this.textContent = 'X';
+  } else if (this.textContent === 'X') {
+    this.textContent = 'O';
+  } else {
+    this.textContent = '';
+  }
+}
+
+for (var i = 0; i < squares.length; i++) {
+  squares[i].addEventListener('click', changeMarker)
+}
+//For loop to add event listeners to all the squares
